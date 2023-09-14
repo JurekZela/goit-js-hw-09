e96f58c8d3223fd55d183e1ab33b0850e4a3c871
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-14 21:10:16 +0200

    task one completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const refBtnStart = document.querySelector('[data-start]');
+const refBtnStop = document.querySelector('[data-stop]');
+const body = document.body;
+
+refBtnStart.addEventListener('click', onClickStartChangeColor);
+refBtnStop.addEventListener('click', onClickStopChangeColor);
+
+let intervalChangeColor = null;
+
+function onClickStartChangeColor() {
+    refBtnStart.disabled = true;
+    refBtnStop.disabled = false;
+
+     intervalChangeColor = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        body.style.backgroundColor = randomColor;
+    }, 1000);
+};
+
+function onClickStopChangeColor() {
+    refBtnStart.disabled = false;
+    refBtnStop.disabled = true;
+    clearInterval(intervalChangeColor);
+};
