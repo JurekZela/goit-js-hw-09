c759ca102d67c92f96cde1d3db9b3d8c1b8d97bc
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-17 15:04:50 +0200

    promise add framework, add logic

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refFormSubmit = document.querySelector('.form');
+const refDelay = refFormSubmit.querySelector('[name="delay"]');
+const refStep = refFormSubmit.querySelector('[name="step"]');
+const refAmount = refFormSubmit.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return  new Promise((resolve, reject) => {
+
+  setTimeout(() => {
+
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({position, delay});
+    } else {
+      reject({position, delay});
+    };
+
+  }, delay);
+
+});
+};
+
+refFormSubmit.addEventListener('submit', createPromise);
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
