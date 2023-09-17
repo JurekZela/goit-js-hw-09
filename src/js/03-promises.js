4f4f2462ecfc38030cbd6c26422e0c1a1f586c2f
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-17 17:50:18 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,32 +1,38 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refForm = document.querySelector('.form');
+const form = document.querySelector(".form");
 
-const delay = refForm.querySelector('[name="delay"]');
-const step = refForm.querySelector('[name="step"]');
-const amount = refForm.querySelector('[name="amount"]');
-
-refForm.addEventListener('submit', createPromise);
-
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({position, delay});
-      } else {
-        reject({position, delay});
-      };
-    }, delay);
-  });
-};
+function onStartCreatePromises(e) {
+  e.preventDefault();
   
+  const delay = parseInt(form.querySelector("[name='delay']").value);
+  const step = parseInt(form.querySelector("[name='step']").value);
+  const amount = parseInt(form.querySelector("[name='amount']").value);
+  
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        };
+      }, delay);
+    });
+  };
+  
+  for (let i = 1; i <= amount; i+= 1) {
+    const timeNextDelay = delay + (i - 1) * step;
+    
+    createPromise(i, timeNextDelay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  };
+};
 
-  createPromise()
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+form.addEventListener("submit", onStartCreatePromises);
